da41e29b0ea3299c7e6d888ddbdc2e8e85ada40b
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-01-10 17:37:58 +0100

    feat: MILESTONE2, JS upgrade

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,76 @@
 console.log("JS OK");
 
-const journeyKmElement = document.getElementById("journey-km");
+const customerNameInputElement = document.getElementById("customer-name");
+const journeyKmInputElement = document.getElementById("journey-km");
 const ageSelectorElement = document.getElementById("age-selector");
 const finalPriceElement = document.getElementById("final-price");
 const btnGenerateElement = document.getElementById("btn-generate");
 
-btnGenerateElement.addEventListener("click", function () {
-  const journeyKm = parseInt(journeyKmElement.value);
-  console.log(journeyKm);
-  const userAge = ageSelectorElement.value;
-  console.log(userAge);
+const ticketElement = document.getElementById("ticket");
+const subtotalValueElement = document.getElementById("subtotal-value");
+const pricePerKmElement = document.getElementById("price-per-km-value");
+const discountLabelElement = document.getElementById("discount-label");
+const discountValueElement = document.getElementById("discount-value");
+const customerNameOutputElement = document.getElementById("customer-detail-name");
+const journeyKmOutputElement = document.getElementById("customer-detail-km");
+const ticketNameElement = document.getElementById("ticket-name");
+const ticketSeatElement = document.getElementById("ticket-seat");
+const ticketCodeElement = document.getElementById("ticket-code");
+const errorElement = document.getElementById("error");
 
-  if (!isNaN(journeyKm)) {
+ticketElement.style.display = "none";
+
+btnGenerateElement.addEventListener("click", function () {
+  discountLabelElement.style.display = "none";
+  discountValueElement.style.display = "none";
+  const customerName = customerNameInputElement.value;
+  const journeyKm = parseInt(journeyKmInputElement.value);
+  const userAge = ageSelectorElement.value;
+  if (!isNaN(journeyKm) && customerName) {
+    ticketElement.style.display = "";
+    errorElement.style.display = "none";
+    let ticketName = "Biglietto Intero";
     const pricePerKm = 0.21;
     let discount = 0;
+    let discountValue = 0;
     const subtotalPrice = pricePerKm * journeyKm;
 
     if (userAge == "under18") {
+      ticketName = "Biglietto Ridotto";
       discount = 20;
+      discountValue = (subtotalPrice * discount) / 100;
+      discountLabelElement.style.display = "";
+      discountValueElement.style.display = "";
+
+      discountLabelElement.innerText = "Sconto Under18 (" + discount + "%):";
+      discountValueElement.innerText = discountValue.toFixed(2) + " €";
     } else if (userAge == "over65") {
+      ticketName = "Biglietto Over65";
       discount = 40;
+      discountValue = (subtotalPrice * discount) / 100;
+      discountLabelElement.style.display = "";
+      discountValueElement.style.display = "";
+
+      discountLabelElement.innerText = "Sconto Over (" + discount + "%):";
+      discountValueElement.innerText = discountValue.toFixed(2) + " €";
     }
 
-    const finalPrice = subtotalPrice * (1 - discount / 100);
+    const finalPrice = subtotalPrice - discountValue;
+
+    subtotalValueElement.innerText = subtotalPrice.toFixed(2) + " €";
+    pricePerKmElement.innerText = pricePerKm.toFixed(2) + " € / Km";
     finalPriceElement.innerText = finalPrice.toFixed(2) + " €";
+    customerNameOutputElement.innerText = customerName;
+    ticketNameElement.innerText = ticketName;
+    journeyKmOutputElement.innerText = journeyKm + " Km";
+
+    const randomSeatNumber = Math.floor(Math.random() * 200) + 1;
+    const randomSeatLetter = (Math.floor(Math.random() * 25) + 10).toString(36);
+    ticketSeatElement.innerText = randomSeatNumber + randomSeatLetter.toUpperCase();
+    ticketCodeElement.innerText = Math.random().toString(36).slice(2);
   } else {
-    finalPriceElement.innerText = "Dati non validi";
+    ticketElement.style.display = "none";
+    errorElement.innerText = "Dati non validi";
+    errorElement.style.display = "";
   }
 });
